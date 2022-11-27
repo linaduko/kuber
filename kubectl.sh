2074591ff0a43392d9123e9cb0b0ebc37391db62
Lina Duko <lina.duko@gmail.com>
2022-11-28 00:24:27 +0300

    sdfghj

--- kubectl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo apt-get -y install curl
-curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
-sudo chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl
-sudo apt-get -y install bash-completion
-echo 'source <(kubectl completion bash)' >>~/.bashrc
-echo 'alias k=kubectl' >>~/.bashrc
-echo 'complete -F __start_kubectl k' >>~/.bashrc
