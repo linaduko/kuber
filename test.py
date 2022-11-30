f4cc3e1ddc2d0faaaad91504789ec6dd4886ed8b
Lina Duko <lina.duko@gmail.com>
2022-11-30 12:10:21 +0300

    hjj

--- test.py
+++ test.py
@@ -5,26 +5,32 @@ print("CHECK REQUIRED FIELDS IN WORDPRESS-DEPLOYMENT.YAML...\n \n ")
 
 f = open('wordpress-deployment.yaml', mode='r')
 file = f.read()
-if "apiVersion:" in file:
+if "apiVersion:v1" in file:
     sleep(2)
     print("check apiVersion...")
     sleep(2)
     print("...ok")
+else:
+    print("ERROR IN WORDPRESS-DEPLOYMENT FILE: incorrect aptVersion")
+    raise SystemExit()
 if "kind:" in file:
     sleep(2)
     print("check kind...")
     sleep(2)
     print("...ok")
+else:
+    raise SystemError()
 if "metadata:" in file:
     sleep(2)
     print("check metadata...")
     sleep(2)
-    print("...ok") 
+    print("...ok")
+else:
+    raise SystemError() 
 if "spec" in file:
     sleep(2)
     print("check spec...")
     sleep(2)
-    print("...ok")  
+    print("...ok")
 else:
-    print("ERROR IN WORDPRESS-DEPLOYMENT FILE")
-    raise SystemError()
+    raise SystemError()  
