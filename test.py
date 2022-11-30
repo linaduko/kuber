b194d6d26bbd543aa279ad61919e1607789cb93d
Lina Duko <lina.duko@gmail.com>
2022-11-30 11:41:44 +0300

    test

--- test.py
+++ test.py
@@ -3,8 +3,7 @@ print("HI \nWelcome to my simple test!")
 f = open('wordpress-deployment.yaml', mode='r')
 line = f.readlines()
 m = line[0]
-try:
-    if "v2" in m:
-        print("OK")
-    except:
-        print('ERROR:\n', traceback.format_exc())
+if "v1" in m:
+       print("OK")
+else:
+    raise SystemError()
